85d05f0e1f6f643d9d93ed088d30b2ee5868e77d
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-01 16:36:11 +0100

    Kodilla task 8.4

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+﻿// scripts.js
+
+//looks stupid but they ask for two so one more to go
+/* a block comment for entire script
+var name = prompt('Enter your name');
+// as instructed puttiun comment it alert line-     alert('Hello, ' + name);
+// and final one finally-           console.log('Hello, ' + name);
+*/
