0b6d035efd10ca2e5f3de866648ac3c1267473c6
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-01 19:34:19 +0100

    task-8.5

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ var name = prompt('Enter your name');
 // as instructed puttiun comment it alert line-     alert('Hello, ' + name);
 // and final one finally-           console.log('Hello, ' + name);
 */
+
+//script for task 8.5
+alert("Welcome to my page that will calculate area covered by a triangle of your choice just type in values and clap your hands while this 'state of the art' computer will perform this super complex operation...")
+var	a = prompt ('type in base length'),
+	h = prompt ('type in height'),
+	triangleArea = a*h/2;
+alert(triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
