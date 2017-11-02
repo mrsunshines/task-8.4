62b0ab7675b3deee293f599c363ec796a3c93220
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-02 18:37:54 +0100

    8.6

--- js/scripts.js
+++ js/scripts.js
@@ -22,14 +22,13 @@ var a = prompt ('Type in value of a'),
 
 alert(value);
 
-if (value >= 0) {
+if (value > 0) {
     console.log('Positive');
 } else if (value < 0) {
     console.log('Negative');
-} else if (value = 0) {
+} else if (value == 0) {
     console.log('Looks like 0');
-} else {
-    console.log('That is no Math');
+} else { console.log('That is no Math');
 }
 
 
